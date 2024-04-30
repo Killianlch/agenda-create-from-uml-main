96652162dc8b189d087880f0d2da242bbc49d008
Killianlch <killian.lechevalier@gmail.com>
2024-04-30 10:05:30 +0200

    add mongodb

--- src/app.js
+++ src/app.js
@@ -7,3 +7,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/agendaDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
